65f09ed200be51d8095dde268f00cba27363cb9e
StoneRiga <stoneriga@gmail.com>
2022-01-06 17:49:50 +0300

    Creation of ItemFActory tinker for user creation

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    use HasFactory;
+
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
